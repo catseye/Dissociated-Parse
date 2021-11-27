9d843d18872ed109f124188c1804408fe64b9c32
Chris Pressey <cpressey@catseye.tc>
2021-11-27 19:53:25 +0000

    Some minor, late-edition adjustments to the cleanup script.

--- cleanup.py
+++ cleanup.py
@@ -7,15 +7,19 @@ def main():
         line = line.replace('..', '.')
         line = line.replace('.,', ',')
         line = line.replace(" 's", "'s")
+        line = line.replace('..', '.')
         for letter in string.ascii_lowercase:
             line = line.replace(". " + letter, ".  " + letter.upper())
         if line.startswith(', '):
             line = 'Lo' + line
         if 'gloom' in line:
-            line = line.rstrip() + '  _' + line.rstrip() + '_\n'
+            line = line.rstrip()
+            line2 = line.replace('gloomy', 'cheesy')
+            line2 = line2.replace('gloom', 'cheese')
+            line = line + '  _' + line2 + '_\n'
         sys.stdout.write(line)
 
 
 if __name__ == '__main__':
-    # MAKE MINE A 99
+    # Brak, I beg to disagree; CHEESE LOG is the log for me!
     main()
